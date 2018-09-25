89ff0ecb7cf918757cbb2308f0da703bbc55dec7
Megan Eagan <Megan.Eagan@gmail.com>
2018-09-24 20:52:27 -0400

    fixed js issues

--- script.js
+++ script.js
@@ -2,75 +2,75 @@ $(document).ready(() => {
 
 //This is the starting budget
 
+let entSpent = parseFloat($("#amountTransaction").val()) || 0;
+let foodSpent = parseFloat($("#amountTransaction").val()) || 0;
+let clothSpent = parseFloat($("#amountTransaction").val()) || 0;
+let billSpent = parseFloat($("#amountTransaction").val()) || 0;
+
+entertainmentBudgetValue = parseFloat($('#entBudgetInput').val()) || 0;
+
+let entRemainSpent = entertainmentBudgetValue - entSpent;
+foodBudgetValue = parseFloat($('#foodBudgetInput').val()) || 0;
+let foodRemainSpent = foodBudgetValue - foodSpent;
+
+clothingBudgetValue = parseFloat($('#clothingBudgetInput').val()) || 0;
+let clothRemainSpent = clothingBudgetValue - clothSpent;
+
+billsBudgetValue = parseFloat($('#billsBudgetInput').val()) || 0;
+let billsRemainSpent = billsBudgetValue - billSpent;
+
 $(".submitBudgetButton").on('click', function(){
-    let entertainmentBudgetValue = parseFloat($('#entBudgetInput').val()) || 0;
+    entertainmentBudgetValue = parseFloat($('#entBudgetInput').val()) || 0;
     let entertainmentBudgetNumber = "$" + entertainmentBudgetValue;
     $('#entertainment-budget').html(entertainmentBudgetNumber);
     
-    let foodBudgetValue = parseFloat($('#foodBudgetInput').val()) || 0;
+    foodBudgetValue = parseFloat($('#foodBudgetInput').val()) || 0;
     let foodBudgetNumber = "$" + foodBudgetValue;
     $('#food-budget').html(foodBudgetNumber);
-
-    let clothingBudgetValue = parseFloat($('#clothingBudgetInput').val()) || 0;
+    
+    clothingBudgetValue = parseFloat($('#clothingBudgetInput').val()) || 0;
     let clothingBudgetNumber = "$" + clothingBudgetValue;
     $('#clothing-budget').html(clothingBudgetNumber);
     
-    let billsBudgetValue = parseFloat($('#billsBudgetInput').val()) || 0;
+    billsBudgetValue = parseFloat($('#billsBudgetInput').val()) || 0;
     let billsBudgetNumber = "$" + billsBudgetValue;
     $('#bills-budget').html(billsBudgetNumber);
-
 
     let totalBudgetNumber = "$" + (entertainmentBudgetValue + foodBudgetValue + clothingBudgetValue + billsBudgetValue);
     document.getElementById('total-budget').innerHTML = totalBudgetNumber;
 
 });
 
+
 //This updates the budget box
 
-let entSpentValue = $("#amountTransaction").val();
-let entSpent = parseFloat(entSpentValue) || 0;
-let foodSpentValue = $("#amountTransaction").val();
-let foodSpent = parseFloat(foodSpentValue) || 0;
-let clothSpentValue = $("#amountTransaction").val();
-let clothSpent = parseFloat(clothSpentValue) || 0;;
-let billSpentValue = $("#amountTransaction").val();
-let billSpent = parseFloat(billSpentValue) || 0;;
-
-let entertainmentBudgetValue = parseFloat($('#entBudgetInput').val()) || 0;
-let entRemainSpent = entertainmentBudgetValue - entSpent;
-
-let foodBudgetValue = parseFloat($('#foodBudgetInput').val()) || 0;
-let foodRemainSpent = foodBudgetValue - foodSpent;
-
-let clothingBudgetValue = parseFloat($('#clothingBudgetInput').val()) || 0;
-let clothRemainSpent = clothingBudgetValue - clothSpent;
-
-let billsBudgetValue = parseFloat($('#billsBudgetInput').val()) || 0;
-let billsRemainSpent = billsBudgetValue - billSpent;
 
 $(".submitTransactionButton").on('click', function(){
     let categoryLog = "hold";
     if ( $('input[id="entertainmentTransaction"]').is(':checked') ) {
-            entSpent += parseFloat($("#amountTransaction").val());
-            entRemainSpent -= entSpent;
+            entSpent += parseFloat($("#amountTransaction").val()) || 0;
+            entRemainSpent = entertainmentBudgetValue -  entSpent;
             $(".entertainment-spent").html('$' + entSpent);
             $(".entertainment-remain").html('$' + entRemainSpent);
             categoryLog = 'Entertainment';
 
 }   else if ($('input[id="foodTransaction"]').is(':checked') ) {
-            foodSpent += parseFloat($("#amountTransaction").val());
+            foodSpent += parseFloat($("#amountTransaction").val()) || 0;
+            foodRemainSpent = foodBudgetValue - foodSpent;
             $(".food-spent").html('$' + foodSpent);
             $(".food-remain").html('$' + foodRemainSpent);
             categoryLog = 'Food';
 
 }   else if ($('input[id="clothingTransaction"]').is(':checked') ) {
-            clothSpent += parseFloat($("#amountTransaction").val());
+            clothSpent += parseFloat($("#amountTransaction").val()) || 0;
+            clothRemainSpent =clothingBudgetValue - clothSpent;
             $(".clothing-spent").html('$' + clothSpent);
             $(".clothing-remain").html('$' + clothRemainSpent);
             categoryLog = 'Clothing';
 
 }   else if ($('input[id="billsTransaction"]').is(':checked') ) {
-            billSpent += parseFloat($("#amountTransaction").val());
+            billSpent += parseFloat($("#amountTransaction").val()) || 0;
+            billsRemainSpent = billsBudgetValue - billSpent;
             $(".bills-spent").html('$' + billSpent);
             $(".bills-remain").html('$' + billsRemainSpent);
             categoryLog = 'Bills';
@@ -78,6 +78,14 @@ $(".submitTransactionButton").on('click', function(){
 }    else {
         console.log("oops");
     };
+
+    
+       
+    let totalSpentNumber = entSpent + foodSpent + clothSpent + billSpent;
+    $('.total-spent').html('$' + totalSpentNumber);
+
+    totalRemainSpent = entRemainSpent + foodRemainSpent + clothRemainSpent + billsRemainSpent;
+    $('.total-remain').html('$' + totalRemainSpent);
 
     //This logs the form input into the transaction log
 
@@ -93,6 +101,19 @@ $(".submitTransactionButton").on('click', function(){
 
 //This is the pop-up that says you are overspent
 
+$(".submitTransactionButton").on('click', function(){
+        if (entSpent > entertainmentBudgetValue) {
+            let budgetAlert = alert('Whoa, buddy. You are over-budget. Stop spending on entertainment!');
+    }   else if (foodSpent > foodBudgetValue) {
+            let budgetAlert = alert('Whoa, buddy. You are over-budget. Stop spending on food!');
+    }   else if (clothSpent > clothingBudgetValue) {
+            let budgetAlert = alert('Whoa, buddy. You are over-budget. Stop spending on clothing!');
+    }   else if (billSpent > billsBudgetValue) {
+            let budgetAlert = alert('Whoa, buddy. You are over-budget. Stop spending on bills!');
+    }   else {
+            console.log("within budget")
+    }
+    });
 
 
-});
+    });
